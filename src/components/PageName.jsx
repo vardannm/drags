ca7b54577d84ffcdeca9dbaabd946766df4b4bb1
vardan.torosyan <vardan.torosyan@daniam.am>
2026-04-22 17:14:29 +0400

    light change

--- src/components/PageName.jsx
+++ src/components/PageName.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function PageName({ title = 'Ընդհանուր տրանսպորտային միջոցներ' }) {
   return (
     <div style={{ fontSize: '18px' }} className="py-1 bg-light text-center text-cs-blue fw-bold">
